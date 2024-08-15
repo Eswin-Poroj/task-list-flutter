fc6c1200f974ac752cc15eba75e5bbcffc16ee69
Eswin Poroj <eporojc1@miumg.edu.gt>
2024-08-14 23:58:26 -0600

    Agregue la pantalla de splashPage

---
+++ lib/app/app.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:task_list/app/view/splashPage.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const primary = Color(0xFF40B7AD);
+    const textColor = Color(0xFF4A4A4A);
+    const background = Color(0xFFF5F5F5);
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          //color en el cual todo sé va a formar la app
+          seedColor: primary,
+        ),
+        //color de fondo de pantalla
+        scaffoldBackgroundColor: background,
+        //Color de texto y fuente para toda la app
+        textTheme: Theme.of(context).textTheme.apply(
+              // declara el tipo de texto
+              fontFamily: 'Poppins',
+              //declara el color del texto en toda la app
+              bodyColor: textColor,
+              //textos de tipos de titulos o subtitulos
+              displayColor: textColor,
+            ),
+        useMaterial3: true,
+      ),
+      home: const SplashPage(),
+    );
+  }
+}
